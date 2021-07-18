53032c6256971801aafa742a950431b988c222f7
Alexey Solovyev <solovevalex@gmail.com>
2021-07-18 14:54:32 -0400

    Adding the TestBase class to the project

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -6,26 +6,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
-public class First {
+public class First extends TestBase {
 
-    String SITE_URL = "https://mvnrepository.com/";
-    WebDriver driver;
-
-    @BeforeEach
-    public void start() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
 
     @Test
     public void firstTest() {
         driver.get(SITE_URL);
     }
 
-    @AfterEach
-    public void finish() {
-        driver.quit();
+    @Test
+    public void secondTest() {
+        driver.get(SITE_URL);
     }
 }
--- src/test/java/Second.java
+++ src/test/java/Second.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-public class Second extends First {
+public class Second extends TestBase {
 
     @Test
     public void firstTest() {
