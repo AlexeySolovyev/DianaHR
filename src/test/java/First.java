729ed50576582792306daf910316d164d567cd89
Alexey Solovyev <solovevalex@gmail.com>
2021-07-18 14:05:00 -0400

    Initial commit

---
+++ src/test/java/First.java
@@ -0,0 +1,31 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+public class First {
+
+    String SITE_URL = "https://mvnrepository.com/";
+    WebDriver driver;
+
+    @BeforeEach
+    public void start() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get(SITE_URL);
+    }
+
+    @AfterEach
+    public void finish() {
+        driver.quit();
+    }
+}
